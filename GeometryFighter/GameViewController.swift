96c461a0b0108f5d0a0b54a9bc6885dd9105ebd4
twcrone <twcrone@gmail.com>
2016-11-26 12:43:02 -0500

    Add code for Chapter 3: Physics

--- GeometryFighter/GameViewController.swift
+++ GeometryFighter/GameViewController.swift
@@ -47,32 +47,53 @@ class GameViewController: UIViewController {
     func setupCamera() {
         cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+        cameraNode.position = SCNVector3(x: 0, y: 5, z: 10)
         scnScene.rootNode.addChildNode(cameraNode)
     }
     
     func spawnShape() {
         var geometry: SCNGeometry
         switch ShapeType.random() {
-            case ShapeType.torus:
-                geometry = SCNTorus()
-                break
-            case ShapeType.capsule:
-                geometry = SCNCapsule()
-                break
-            case ShapeType.cone:
-                geometry = SCNCone()
-                break
-            case ShapeType.cylinder:
+            
+        case ShapeType.torus:
+            geometry = SCNTorus()
+            break
+            
+        case ShapeType.capsule:
+            geometry = SCNCapsule()
+            break
+
+        case ShapeType.cone:
+            geometry = SCNCone()
+            break
+            
+        case ShapeType.tube:
+            geometry = SCNTube()
+            break
+
+        case ShapeType.pyramid:
+            geometry = SCNPyramid()
+            break
+
+        case ShapeType.cylinder:
                 geometry = SCNCylinder()
                 break
-            case ShapeType.sphere:
-                geometry = SCNSphere(radius: 1.0)
-                break
-            default:
-                geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        
+        case ShapeType.sphere:
+            geometry = SCNSphere(radius: 1.0)
+            break
+
+        default:
+            geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
         }
+        geometry.materials.first?.diffuse.contents = UIColor.random()
         let geometryNode = SCNNode(geometry: geometry)
+        geometryNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        let randomX = Float.random(min: -2, max: 2)
+        let randomY = Float.random(min: 10, max: 18)
+        let force = SCNVector3(x: randomX, y: randomY, z: 0)
+        let position = SCNVector3(x: 0.05, y: 0.05, z: 0.05)
+        geometryNode.physicsBody?.applyForce(force, at: position, asImpulse: true)
         scnScene.rootNode.addChildNode(geometryNode)
     }
 }
